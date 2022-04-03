d624b4291d24e4386122898d3d9b480a725cf152
BlackExcalibur <domeyeben4@gmail.com>
2022-04-03 23:47:22 +0000

    bafoe updated optimized

--- bafoe.py
+++ bafoe.py
@@ -1,19 +1,19 @@
-start = True
-stop = True
+start = True # Start the game with a boolean value true
 
-while True:
+
+while True: # While the game is running
     command = input('>> ').lower()
     if command == 'start':
-        if start:
+        if start: # If game is running and user just typed in start
             print('Car has started')
-            start = False
-        elif not start and command == 'start':
+            start = False # Set start to false so it doesn't run again
+        elif not start and command == 'start': # If game is running and user typed in start again
             print('Car has already started')
     elif command == 'stop':
-        if stop:
+        if not start: # If game is running and user typed in stop
             print('Car has stopped')
-            stop = False
-        elif not stop and command == 'stop':
+            start = True # Set start to true so it doesn't run again in this block
+        elif start and command == 'stop': # If game is running and user typed in stop again
             print('Car has already stopped')
     else:
         print('Err: command not found')
