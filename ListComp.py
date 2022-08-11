61858e23374c70edd5b4d9c5399bdd61aa4d7097
BlackExcalibur <domeyeben4@gmail.com>
2022-08-11 18:28:46 +0000

    sorted python

--- ListComp.py
+++
@@ -1,7 +0,0 @@
-#list comprehensions are used to produce iterations on a list
-#a longer version of such iterations are
-s = "This is a trial string"
-h = []
-for a in s:
-    h.append(a)
-print(h)
--- Third.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-tam = np.array([1, 2, 3, 4, 5, 6, 7, 8])
-tr = tam.reshape(2, 2, -1)
-print(tr)
-print(tr.base)
-yam = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-ur = yam.reshape(-1)
-print(yam)
-print(ur)
