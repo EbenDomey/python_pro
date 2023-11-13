2625be8312027e62bb0cc0f87d324f92982192c8
EbenDomey <domeyeben4@gmail.com>
2023-11-13 20:21:03 +0000

    done

--- exercises/correctionCode.py
+++
@@ -1,7 +0,0 @@
-try:
-    t = int(input("Input your number: "))
-except ValueError:
-    print("INVALID INPUT!!!NUMBERS ONLY")
-    exit()
-
-print(f"your number is {t}")
--- ooPython/errorTesting.py
+++
@@ -1,5 +0,0 @@
-a= 5
-b=10
-print(a)
-#print(B) for pylint tuts
-print(b)
